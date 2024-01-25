bc5566642e52d3224ba8847a270b621a1986b8d0
karinadarc <darcsilva.kd@gmail.com>
2024-01-24 22:21:09 -0300

    Adiciona UserModel

---
+++ src/database/UserDataBase.ts
@@ -0,0 +1,18 @@
+import { UserDBModel } from "../models/User";
+import { BaseDatabase } from "./BaseDataBase";
+
+export class UserDatabase extends BaseDatabase {
+  public addUser = async (user: UserDBModel): Promise<void> => {
+    await BaseDatabase.connection.insert(user).into(this.TABLE_USERS);
+  };
+
+  public getByEmail = async (
+    email: string
+  ): Promise<UserDBModel | undefined> => {
+    return await BaseDatabase.connection
+      .select()
+      .from<UserDBModel>(this.TABLE_USERS)
+      .where({ email })
+      .first();
+  };
+}
