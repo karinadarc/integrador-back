5b1e6a7c37f5ae1ef3f05dcad6dcbe0210eb00a7
karinadarc <darcsilva.kd@gmail.com>
2024-02-21 20:18:50 -0300

    06 - Adiciona listagem de posts

--- tests/mocks/PostDabataseMock.ts
+++ tests/mocks/PostDabataseMock.ts
@@ -1,8 +1,23 @@
-import { PostDbModel } from "../../src/models/Post";
+import { PostDbModel, PostWithCreatorDbModel } from "../../src/models/Post";
 import { BaseDatabaseMock } from "./BaseDatabaseMock";
 
 export class PostDatabaseMock extends BaseDatabaseMock {
   public addPost = async (post: PostDbModel): Promise<string> => {
     return post.id;
+  };
+
+  public getAllPosts = async (): Promise<PostWithCreatorDbModel[]> => {
+    return [
+      {
+        id: "a7102751-26c4-4606-bbf4-b4d97dae37c2",
+        content: "content",
+        likes: 0,
+        dislikes: 0,
+        created_at: "2024-02-12T01:03:03.080Z",
+        updated_at: "2024-02-12T01:03:03.080Z",
+        creator_id: "11111111-1111-1111-1111-111111111111",
+        creator_name: "Fulano",
+      },
+    ];
   };
 }
