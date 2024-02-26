35f9a697de8f71e2f21c4ea9616043ca7c789ed5
karinadarc <darcsilva.kd@gmail.com>
2024-02-26 00:48:23 -0300

    Adiciona testes like e dislike

--- tests/mocks/PostDabataseMock.ts
+++ tests/mocks/PostDabataseMock.ts
@@ -1,3 +1,4 @@
+import { LikeDislikeDbModel } from "../../src/models/LikeDislike";
 import { PostDbModel, PostWithCreatorDbModel } from "../../src/models/Post";
 import { BaseDatabaseMock } from "./BaseDatabaseMock";
 
@@ -28,8 +29,8 @@ export class PostDatabaseMock extends BaseDatabaseMock {
     return {
       id,
       content: "content",
-      likes: 0,
-      dislikes: 0,
+      likes: 1,
+      dislikes: 1,
       created_at: "2024-02-12T01:03:03.080Z",
       updated_at: "2024-02-12T01:03:03.080Z",
       creator_id: "11111111-1111-1111-1111-111111111111",
@@ -39,4 +40,34 @@ export class PostDatabaseMock extends BaseDatabaseMock {
   public updatePost = async (post: PostDbModel): Promise<void> => {};
 
   public deletePost = async (post: PostDbModel): Promise<void> => {};
+
+  public async getPostlikeDeslikeByUser(
+    postId: string,
+    userId: string
+  ): Promise<LikeDislikeDbModel | undefined> {
+    if (postId === "like-not-found") {
+      return undefined;
+    }
+    return {
+      user_id: "11111111-1111-1111-1111-111111111111",
+      post_id: postId,
+      like: Number(postId === "liked"),
+    };
+  }
+
+  public async updatePostandAddLikeDislike(
+    post: PostDbModel,
+    likeDislike: LikeDislikeDbModel
+  ): Promise<void> {}
+
+  public async updatePostAndRemoveLikeDislike(
+    post: PostDbModel,
+    likeDislike: LikeDislikeDbModel
+  ): Promise<void> {}
+
+  public async updatePostAndReplaceLikeDislike(
+    post: PostDbModel,
+    newLikeDislike: LikeDislikeDbModel,
+    oldLikeDislike: LikeDislikeDbModel
+  ): Promise<void> {}
 }
